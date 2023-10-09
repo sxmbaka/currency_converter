83f77c269da3fdbc8451f008ee9bd16b575f14d8
sxmbaka <yatharth.singhhh@gmail.com>
2023-10-10 00:07:26 +0530

    update README.md and currency_converter_material_page.dart

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,19 +6,27 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: ColoredBox(
-          color: Color.fromRGBO(152, 255, 8, 0.211),
+          color: Color.fromRGBO(48, 48, 44, 0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Text(
                 'Hello Mini!',
                 style: TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
+                  color: Color.fromARGB(255, 255, 255, 255),
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  label: Text(
+                    'Enter Currency',
+                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
                 ),
               ),
             ],
