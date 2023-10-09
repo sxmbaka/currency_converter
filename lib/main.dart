2740bd5f0b26665775278267784efc5194b7f429
sxmbaka <yatharth.singhhh@gmail.com>
2023-10-09 18:46:31 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text('Hello, world!'));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello, world!!!!!!'
+          )
+        )
+      )
+    );
+    // return const Text(
+    //   'Hello, world!!!!!!',
+    //   textDirection: TextDirection.ltr,
+    // );
+  }
 }
